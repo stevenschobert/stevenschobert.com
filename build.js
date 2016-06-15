221607e72913a265e2f5d84172dbaa3cd2b30919
Steven Schobert <spschobert@gmail.com>
2016-06-14 23:54:28 -0500

    restructure build file

--- build.js
+++ build.js
@@ -24,84 +24,90 @@ var rename            = require("./lib/rename");
 // build state helpers
 var startTime = Date.now();
 
-Metalsmith(__dirname)
-  .use(ignore(["**/.DS_Store"]))
+var pipeline = Metalsmith(__dirname);
+
+// misc
+pipeline.use(ignore(["**/.DS_Store"]))
 
   // content
-  .use(inkplate({
-    extension: "md",
-    apiKey: process.env.INKPLATE_API_KEY,
-    host: process.env.INKPLATE_HOST,
-    processPost: function(post) {
-      return {
-        collection: "posts",
-        layout: "default.haml",
-        title: post.title,
-        type: "post",
-        draft: !!(post.status !== "publish"),
-        date: new Date(post.created_at),
-        color: (post.custom_fields || {}).color,
-        link: (post.custom_fields || {}).link
-      };
-    },
-    processPage: function(page) {
-      return {
-        draft: !!(page.status !== "publish"),
-        layout: "default.haml",
-        type: "page",
-        title: page.title
-      };
-    }
-  }))
-  .use(metallic())
-  .use(markdown())
+pipeline.use(inkplate({
+  extension: "md",
+  apiKey: process.env.INKPLATE_API_KEY,
+  host: process.env.INKPLATE_HOST,
+  processPost: function(post) {
+    return {
+      collection: "posts",
+      layout: "default.haml",
+      title: post.title,
+      type: "post",
+      draft: !!(post.status !== "publish"),
+      date: new Date(post.created_at),
+      color: (post.custom_fields || {}).color,
+      link: (post.custom_fields || {}).link
+    };
+  },
+  processPage: function(page) {
+    return {
+      draft: !!(page.status !== "publish"),
+      layout: "default.haml",
+      type: "page",
+      title: page.title
+    };
+  }
+}));
+pipeline.use(metallic());
+pipeline.use(markdown());
 
-  // structure
-  .use(drafts())
-  .use(collections({
-    posts: {
-      sortBy: "date",
-      reverse: true
-    }
-  }))
-  .use(archive({
-    collections: ["\\d{4}\\/\\d{2}\\/\\d{2}\\/*"]
-  }))
-  .use(permalink())
-  .use(rootPath())
-  .use(paths())
+// remove drafts in production
+if (process.env.NODE_ENV === "production") {
+  pipeline.use(drafts());
+}
+
+// structure
+pipeline.use(collections({
+  posts: {
+    sortBy: "date",
+    reverse: true
+  }
+}));
+pipeline.use(archive({
+  collections: ["\\d{4}\\/\\d{2}\\/\\d{2}\\/*"]
+}));
+pipeline.use(permalink());
+pipeline.use(rootPath());
+pipeline.use(paths());
 
   // rendering
-  .use(sass({
-    outputStyle: "compressed"
-  }))
-  .use(includes({
-    directory: "includes",
-    pattern: "**/*.haml",
-    preserveWhitespace: true,
-    matcher: "-# include (.*)"
-  }))
-  .use(helpers())
-  .use(inPlace({
-    engine: "haml",
-    pattern: ["**/*.haml"]
-  }))
-  .use(layouts({
-    engine: "haml",
-    directory: "layouts"
-  }))
-  .use(rename(/^(.*)\.haml$/i, "$1"))
-  .use(relocateUploads())
+pipeline.use(sass({
+  outputStyle: "compressed"
+}));
+pipeline.use(includes({
+  directory: "includes",
+  pattern: "**/*.haml",
+  preserveWhitespace: true,
+  matcher: "-# include (.*)"
+}));
+pipeline.use(helpers());
+pipeline.use(inPlace({
+  engine: "haml",
+  pattern: ["**/*.haml"]
+}));
+pipeline.use(layouts({
+  engine: "haml",
+  directory: "layouts"
+}));
+pipeline.use(rename(/^(.*)\.haml$/i, "$1"));
+pipeline.use(relocateUploads());
 
-  // finalize
-  .build(function resolveBuild(error) {
-    var endTime = Date.now();
-    var elapsedSeconds = (endTime - startTime) / 100;
+// finalize
+pipeline.build(function resolveBuild(error) {
+  var endTime = Date.now();
+  var elapsedSeconds = (endTime - startTime) / 100;
 
-    if (error) {
-      console.log(error);
-      throw error;
-    } else {
-      process.stdin.write("Done building! Build time: " + elapsedSeconds + " seconds");
-    }
-  });
+  if (error) {
+    console.log(error);
+    throw error;
+  } else {
+    process.stdin.write("Done building! Build time: " + elapsedSeconds + " seconds");
+  }
+});
