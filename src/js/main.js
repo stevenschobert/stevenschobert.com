47b587b4be9100606a8982764c202781f6cb6b19
Steven Schobert <spschobert@gmail.com>
2016-10-28 16:19:28 -0500

    update blog design

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,4 @@
 (function() {
-  'use strict';
+  "use strict";
 
-  var images = document.querySelectorAll('.text-block p > img');
-
-  if (images.length > 0) {
-    [].forEach.call(images, function(image) {
-      image.parentNode.setAttribute('class', 'full-image');
-    });
-  }
 }());
