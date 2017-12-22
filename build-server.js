b7b1f22480ceb3ea61978852644cef4e10b5ebdf
Steven Schobert <spschobert@gmail.com>
2017-12-22 15:38:10 -0600

    poll api for auto-building

--- build-server.js
+++ build-server.js
@@ -1,8 +1,19 @@
+require("dotenv").config();
+
 const http = require("http");
 const childProcess = require("child_process");
+const request = require("request");
 const spawn = childProcess.spawn;
 
-function runBuild(cb) {
+var runningBuild = false;
+var checkingBuild = false;
+var lastBuildTimestamp = null;
+
+function runBuild(buildTimestamp) {
+  if (runningBuild) { return; }
+
+  runningBuild = true;
+
   var buildPs = spawn("node", [ "build.js" ]);
   var stdout = "";
   var stderr = "";
@@ -16,42 +27,60 @@ function runBuild(cb) {
   });
 
   buildPs.on("close", (code) => {
-    cb(code, stdout, stderr);
+    runningBuild = false;
+    console.log("build finished, exit code: " + code);
+
+    if (code == 0) {
+      lastBuildTimestamp = buildTimestamp;
+    }
   });
 }
 
-const server = http.createServer(function(req, res) {
-  if ((req.url == "/build" || req.url == "/build/") && req.method == "POST") {
-    if (req.headers && req.headers["x-api-key"] == process.env.BUILD_SERVER_API_KEY) {
-      runBuild((code, stdout, stderr) => {
-        const result = {
-          success: code == 0,
-          code: code,
-          stdout: stdout,
-          stderr: stderr
-        };
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "application/json");
-        res.end(JSON.stringify(result));
-      });
-    } else {
-      res.statusCode = 400;
-      res.end("Bad request.");
-    }
-  } else {
-    res.statusCode = 404;
-    res.end("Not found.");
-  }
-});
+function checkBuild() {
+  if (checkingBuild) { return; }
 
-console.log("running initial build...");
-runBuild((code, stdout, stderr) => {
-  var port = process.env.PORT || 3000;
-  if (code != 0) {
-    console.log("initial build failed: " + stderr);
-  } else {
-    console.log("initial build succeeded: " + stdout);
-    server.listen(port);
-    console.log("listening on port " + port);
-  }
-});
+  checkingBuild = true;
+
+  var host = process.env.INKPLATE_HOST;
+  var key = process.env.INKPLATE_API_KEY;
+
+  var path = host + "/api/status?api_key=" + key;
+
+  request(path, function (error, response, body) {
+    checkingBuild = false;
+
+    if (error) {
+      console.log("error getting status: " + error);
+      return;
+    }
+
+    if (response.statusCode != 200) {
+      console.log("bad status: " + response.statusCode);
+      return;
+    }
+
+    if (!response.body) {
+      console.log("missing response body: " + response.body);
+      return;
+    }
+
+    var body = null;
+    try {
+      body = JSON.parse(response.body);
+    } catch(error) {
+      console.log("error parsing body: " + error);
+    }
+
+    if (!body || !body.last_updated_at) {
+      console.log("last_updated_at not found: " + body);
+    }
+
+    if (body.last_updated_at != lastBuildTimestamp) {
+      console.log("New timestamp, starting build: " + body.last_updated_at);
+      runBuild(body.last_updated_at);
+    }
+  });
+}
+
+checkBuild();
+setInterval(checkBuild, parseInt(process.env.BUILD_CHECK_INTERVAL || 5000));
